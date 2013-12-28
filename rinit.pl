081d9059c050c4faf8a637f12a2300abd0517074
alexbhandari <alexbhandari@gmail.com>
2013-12-28 01:32:10 -0500

    Started controller generator. Need to add test suite setup

--- rinit.pl
+++ rinit.pl
@@ -2,33 +2,59 @@ use strict;
 use warnings;
 use diagnostics;
 
-#initialization variables
+# shell color
+my $red    = "\033[31m";
+my $green  = "\033[32m";
+my $yellow = "\033[33m";
+my $blue   = "\033[34m";
+my $clear  = "\033[0m";
+my $italic = "\033[3m";
+my $blink  = "\033[5m";
+
+# initialization variables
+my $required_rvm = 'ruby-2.0.0-p353@railstutorial_rails_4_0';
 use Cwd 'abs_path';
 my $name = $ARGV[0];
 my $script_path = abs_path();
 my %copy_files = ("Gemfile"=>".");
 
-#print pre-run information
-print "\033[32m";
+# check rvm gemset
+print $green."checking rvm gemset...\n";
+if (!(`rvm-prompt` eq $required_rvm . "\n")) {
+   print "This script requires gems (might be just rails) known to be contained in the rvm-gemset:\n";
+   print $required_rvm . ' on alex@macbook' . "\n";
+   print $red."Your current gemset is\n" . `rvm-prompt` . "or rvm is unsupported on your system\n";
+   print "This script is untested in this environment.\n";
+   print $yellow."Would you like to continue? [yes/return to abort]\n";
+   exit 0 if !(<STDIN> eq "yes\n");
+   print $green."Continuing...\n";
+}
+else {
+   print "Enviromnent Supported.\n";
+}
+
+# print pre-run information
+print $green;
 print "Script path:" . abs_path($0) . "\n";
 print "Working in " . `pwd`;
 print "Program: \Urails\n\033[0m";
 if ($name eq "") {
-   print "\033[31mApp name argument not specified.\n";
+   print $red."App name argument not specified.\n";
    print "Usage: rails_init [AppName]\n";
    print "Terminating..\n";
    exit 0;
 }
-print "\033[34mJob: rails init for $name\n";
+print $blue;
+print "Job: rails init for $name\n";
 print "Config with Git\nDeployment: Heroku\n";
 print "Development: sqlite\nProduction: postgres\n";
 
-print "\033[0;3;5mPress enter to continue. Input any character to exit.\033[0m";
+print $clear.$italic.$blink."Press enter to continue. Input any character to exit.$clear";
 exit 0 if !(<STDIN> eq "\n");
 
 #generate command array
 #copy template files: all stored in files subdirectory
-my @commands = ("rails new $name","cd $name");
+my @commands = ("rails new $name");
 my @git_init = ("git init","touch README", "git add .", "git commit -m 'initialized using rails script'",
                 'git remote add origin git@github.com:alexbhandari/'."$name.git", "git push -u origin master",
                 "heroku create");
@@ -38,7 +64,7 @@ while (my($file, $loc) = each %copy_files) {
 }
 push(@commands, "bundle install");
 
-#run commands
+# Stage 1: Initialization
 system("clear");
 print "run: clear\n";
 foreach (@commands) {
@@ -57,3 +83,26 @@ if (<STDIN> eq "yes\n") {
       system($_);
    }
 }
+# Stage 2: Pages and Testing
+print $green."Initialization Complete\n\033[0m";
+print "-----------------------------------------------------\n";
+print $yellow."Continue with step 2: Setup pages and testing? [yes/no]\n\033[0m";
+exit 0 if !(<STDIN> eq "yes\n");
+print $blue."Generating controllers:\n";
+my $loop = 1;
+while($loop) {
+   print $yellow."$loop) Generate controller #$loop? [yes]\n";
+   last if !(<STDIN> eq "yes\n");
+   print "Enter name\n";
+   my $cname = <STDIN>;
+   print "Enter pages separated by spaces:\n";
+   my $cpages = <STDIN>;
+   my $cmd = "rails generate controller $cname $cpages";
+   print $italic.$blink."Confirm command: ".$clear.$blue." $cmd [enter to confirm/anything else to abort]\n";
+   if (<STDIN> eq "\n") {
+      print $clear."run: $cmd\n";
+      system($cmd);
+      $loop++;
+   }
+}
+print $green."Done!\n";
